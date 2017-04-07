235ccbc52457507650bb499117cc3d06155715d6
fbenavid <fbenalvarez92@gmail.com>
2017-04-07 16:43:28 -0400

    erase the crap

--- app/src/main/java/com/example/sabdelat/languageapp/LandingActivity.java
+++ app/src/main/java/com/example/sabdelat/languageapp/LandingActivity.java
@@ -4,12 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LandingActivity extends AppCompatActivity {
-    double test2;
-    int test;
-    int sondos;
-    boolean tinyRick;
 
-    String myname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
